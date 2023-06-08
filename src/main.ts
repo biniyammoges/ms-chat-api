411c48893d4be4d665db834df1b3d0158e53613c
Biniyam Moges <biniyammoges54@gmail.com>
2023-06-08 22:16:04 +0300

    added swagger and redis subscriber initialization to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { initalizeWsAdapter } from './init-ws-adapter';
+import { initalizeRedisSubscriber } from './initialize-redis-ps';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('main')
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api')
 
   // Initialize new socket adaptor 
   initalizeWsAdapter(app);
+  initalizeRedisSubscriber(app)
+
+  const config = new DocumentBuilder()
+    .setTitle('ms-chat api')
+    .setDescription('ms-chat api documentation')
+    .setVersion('1.0.0')
+    .addTag('ms-chat')
+    .addBearerAuth()
+    .build();
+
+  SwaggerModule.setup('/explorer', app, SwaggerModule.createDocument(app, config));
 
   const port = process.env.PORT || 3000
   await app.listen(port);
+  logger.log(`Server started on port ${port}`)
 }
+
 bootstrap();
